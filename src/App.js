61ac4155fe80576370d62c70476170e055bf87d5
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-04-29 15:35:28 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import GitImage from './githubimage.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container text-center">
+      <h1 className="py-5 text-uppercase"> Git Hub Profile</h1>
+      <form >
+        <div className="form-group">
+          <div className="input-group">
+            <input type="text"
+            className="form-control"
+            required
+            />
+            <span className="input-group-btn">
+              <button type = "submit" className="btn btn-success"> Search</button>
+            </span>
+          </div>  
+        </div>    
+      </form>
+      <div className="py-5">
+        <img src={GitImage} className="responsive rounded-circle" alt="" height="200px"/>
+        <h1 className="pt-5">
+          <a href="https://github.com/Zemiro014" target="_new"> Jeronimo Kulandissa</a>
+        </h1>
+      </div>
     </div>
   );
 }
